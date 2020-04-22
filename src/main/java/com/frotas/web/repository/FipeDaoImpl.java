9ec9cb9ae08e7e462c00d92757629cf89f2ea767
Marcos Rogerio <marcosrogerio.jrf@gmail.com>
2020-04-22 17:11:15 -0400

    Atividade Camada de Serviço

---
+++ src/main/java/com/frotas/web/repository/FipeDaoImpl.java
@@ -0,0 +1,9 @@
+package com.frotas.web.repository;
+
+import com.frotas.web.domain.Fipe;
+
+public class FipeDaoImpl extends AbstractDao<Fipe, Long> 
+						 implements FipeDao {	
+	
+
+}
---
+++ src/main/java/com/frotas/web/repository/VeiculoDaoImpl.java
@@ -0,0 +1,8 @@
+package com.frotas.web.repository;
+
+import com.frotas.web.domain.Veiculo;
+
+public class VeiculoDaoImpl extends AbstractDao<Veiculo, Long>
+							implements VeiculoDao{
+
+}
